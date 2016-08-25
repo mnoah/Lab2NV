4b8dce3918d6fdfb8894926c2f997a1111ef9d94
mnoah <noahvrazo@gmail.com>
2016-08-25 16:37:38 -0400

    method update

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -25,59 +25,64 @@ namespace ConsoleApplication3
             Console.WriteLine("Please select one: Sword, Slingshot or Sick dance moves ");
 
             Awn3 = Console.ReadLine();
+            do {
 
-            if (Awn3 == "Sword")
-            {
-                Console.WriteLine("You draw your sword and swing it hard at the beast!");
-                Console.WriteLine("The beast ducks it's head, but it didnt see you roll to the side and deliver a mighty blow");
-                Console.WriteLine("The beast falls dead. byoned the dead boy you see a pile of gold, you're rich!");
-                Console.WriteLine("congradulations " + Name + " you won the game!");
-            }
-            else if (Awn3 == "Slingshot")
-            {
-                Console.WriteLine("you produce your mighty slingshot and lunch a smooth round stone at the ");
-                Console.WriteLine("The stone bounces off of the mighty beast");
-                Console.WriteLine("You are daed...");
-            }
-            else if (Awn3 == "sick dance moves")
-            {
+                if (Awn3 == "Sword")
+                {
+                    Console.WriteLine("You draw your sword and swing it hard at the beast!");
+                    Console.WriteLine("The beast ducks it's head, but it didnt see you roll to the side and deliver a mighty blow");
+                    Console.WriteLine("The beast falls dead. byoned the dead boy you see a pile of gold, you're rich!");
 
-                Console.WriteLine("you produce your mighty slingshot and lunch a smooth round stone at the ");
-                Console.WriteLine("The stone bounces off of the mighty beast");
-                Console.WriteLine("You are daed...");
-            }
+                }
+                else if (Awn3 == "Slingshot")
+                {
+                    Console.WriteLine("you produce your mighty slingshot and lunch a smooth round stone at the ");
+                    Console.WriteLine("The stone bounces off of the mighty beast");
+                    Console.WriteLine("You are daed...");
+                }
+                else if (Awn3 == "sick dance moves")
+                {
+
+                    Console.WriteLine("you produce your mighty slingshot and lunch a smooth round stone at the ");
+                    Console.WriteLine("The stone bounces off of the mighty beast");
+                    Console.WriteLine("You are daed...");
+                }
+            } while (!(Awn3 == "sword" || Awn3 == "sick dance moves" || Awn3 == "Slingshot"));
         }
 
         public void Move2()
         {
             string Awn3;
+            do
+            {
 
-            Console.WriteLine("2 Heads dawg");
-            Console.WriteLine("2 headed dragons are easy to kill because each head only has half a brain");
-            Console.WriteLine("what kind of weapon would you like to use?");
-            Console.WriteLine("Please select one: Sword, Slingshot or Sick dance moves ");
-            Awn3 = Console.ReadLine();
-            if (Awn3 == "sword")
-            {
-                Console.WriteLine("You draw your sword and point it at the ugly monster!");
-                Console.WriteLine("The beast takes one look at you in your shiny armor and fants");
-                Console.WriteLine("after dispacting the beast on the ground you go for the gold!");
-                Console.WriteLine("congradulations " + Name + " you won the game!");
-            }
-            else if (Awn3 == "Slingshot")
-            {
-                Console.WriteLine("taking out your slingshot you aim at one of the monsters heads");
-                Console.WriteLine("losing the stone at the dragons left head it bounces off and hits the other");
-                Console.WriteLine("The beast is now dead all of his tresurs are now yours");
-                Console.WriteLine("congradulations " + Name + " you won the game!");
-            }
-            else if (Awn3 == "Sick dance moves")
-            {
-                Console.WriteLine("You are taken a back as the clumsy beast starts to dance!");
-                Console.WriteLine("Seeing its awful dancing, you bust out your own moves");
-                Console.WriteLine("The monster is so clumsy it trips over its own heads falling ontop of you");
-                Console.WriteLine("The heavy beast cruses you and you die");
-            }
+                Console.WriteLine("2 Heads dawg");
+                Console.WriteLine("2 headed dragons are easy to kill because each head only has half a brain");
+                Console.WriteLine("what kind of weapon would you like to use?");
+                Console.WriteLine("Please select one: Sword, Slingshot or Sick dance moves ");
+                Awn3 = Console.ReadLine();
+                if (Awn3 == "sword")
+                {
+                    Console.WriteLine("You draw your sword and point it at the ugly monster!");
+                    Console.WriteLine("The beast takes one look at you in your shiny armor and fants");
+                    Console.WriteLine("after dispacting the beast on the ground you go for the gold!");
+                    Console.WriteLine("congradulations " + Name + " you won the game!");
+                }
+                else if (Awn3 == "Slingshot")
+                {
+                    Console.WriteLine("taking out your slingshot you aim at one of the monsters heads");
+                    Console.WriteLine("losing the stone at the dragons left head it bounces off and hits the other");
+                    Console.WriteLine("The beast is now dead all of his tresurs are now yours");
+                    Console.WriteLine("congradulations " + Name + " you won the game!");
+                }
+                else if (Awn3 == "Sick dance moves")
+                {
+                    Console.WriteLine("You are taken a back as the clumsy beast starts to dance!");
+                    Console.WriteLine("Seeing its awful dancing, you bust out your own moves");
+                    Console.WriteLine("The monster is so clumsy it trips over its own heads falling ontop of you");
+                    Console.WriteLine("The heavy beast cruses you and you die");
+                }
+            } while (!(Awn3 == "sword" || Awn3 == "sick dance moves" || Awn3 == "Slingshot"));
 
         }
 
@@ -88,47 +93,63 @@ namespace ConsoleApplication3
             string Awn3;
             Console.WriteLine("3 Heads dawg");
             Console.WriteLine("3 headed dragons have the sickest dance moves and challenges you to a dance off");
-            Console.WriteLine("what kind of weapon would you like to use?");
-            Console.WriteLine("Please select one: Sword, Slingshot or Sick dance moves ");
-            Awn3 = Console.ReadLine();
-            if (Awn3 == "Sword")
+
+
+            do
             {
-                Console.WriteLine("You pull your sword out and swing at the mighty beats");
-                Console.WriteLine("Its dance moves are so smooth you can't seem to get a hit on him");
-                Console.WriteLine("after realizing you dontr intend to do dance with the dragon, he sets you a flame");
-                Console.WriteLine("you are now burnt to a crisp");
-            }
-            else if (Awn3 == "Singshot")
-            {
-                Console.WriteLine("you produce your mighty slingshot and lunch a smooth round stone at the ");
-                Console.WriteLine("The stone bounces off of the mighty beast");
-                Console.WriteLine("You are daed...");
-            }
-            else if (Awn3 == "Sick dance moves")
-            {
-                Console.WriteLine("The monster starts to dance, it's moves are so smooth you almost can't believe your eyes!");
-                Console.WriteLine("Sortly the spell is broken and you rermeber your training.");
-                Console.WriteLine("You start moving like no one has ever moved before.");
-                Console.WriteLine("The dragon can't handle it, your moves are to great.");
-                Console.WriteLine("Losing its will to live the dragon falls over dead");
-                Console.WriteLine("congradulations " + Name + " you won the game!");
-            }
+                Console.WriteLine("what kind of weapon would you like to use?");
+                Console.WriteLine("Please select one: Sword, Slingshot or Sick dance moves ");
+                Awn3 = Console.ReadLine();
+                if (Awn3 == "sword")
+                {
+                    Console.WriteLine("You pull your sword out and swing at the mighty beats");
+                    Console.WriteLine("Its dance moves are so smooth you can't seem to get a hit on him");
+                    Console.WriteLine("after realizing you dontr intend to do dance with the dragon, he sets you a flame");
+                    Console.WriteLine("you are now burnt to a crisp");
+                }
+                else if (Awn3 == "singshot")
+                {
+                    Console.WriteLine("you produce your mighty slingshot and lunch a smooth round stone at the ");
+                    Console.WriteLine("The stone bounces off of the mighty beast");
+                    Console.WriteLine("You are daed...");
+                }
+                else if (Awn3 == "sick dance moves")
+                {
+                    Console.WriteLine("The monster starts to dance, it's moves are so smooth you almost can't believe your eyes!");
+                    Console.WriteLine("Sortly the spell is broken and you rermeber your training.");
+                    Console.WriteLine("You start moving like no one has ever moved before.");
+                    Console.WriteLine("The dragon can't handle it, your moves are to great.");
+                    Console.WriteLine("Losing its will to live the dragon falls over dead");
+
+                }
+                else
+                {
+
+
+                    Console.WriteLine("Please select one");
+
+
+                }
+                
+            } while (!(Awn3 == "sword" || Awn3 == "sick dance moves" || Awn3 == "Slingshot"));
         }
-        class Program
+       
+      
+      class Program
         {
             static void Main(string[] args)
             {
                 MovesMain Moves = new MovesMain();
 
 
-                string Name;
+                
 
                 string FinalAwn;
                 do
                 {
-
+                    string Name;
                     string Awn;
-                    
+
 
 
                     Console.WriteLine("Welcome! What is your name?: ");
@@ -157,7 +178,7 @@ namespace ConsoleApplication3
                                 int Awn2Num = int.Parse(Awn2);
 
 
-                                if (Awn2Num == 1)
+                                if (Awn2Num == 1) // Move 1
                                 {
                                     Moves.Move1();
                                 }
@@ -175,6 +196,8 @@ namespace ConsoleApplication3
                                     Moves.Move3();
                                 }
 
+                                
+
                             }
                             else if (fightOrFlight == "runaway")
                             {
@@ -189,44 +212,17 @@ namespace ConsoleApplication3
                             Console.WriteLine("You make me sad, goodbye");
                             return;
                         }
-                        else
+                        else 
                         {
                             Console.WriteLine("Please anwser yes or no");
                         }
-                    }
-                    while (Awn != "yes");
-                    while (Awn != "no") ;
 
+                        Console.WriteLine("Game over " + Name + " you finished the game!");
 
-
-
-
-
-
+                    }while (!(Awn == "no" || Awn == "yes" ));
 
                     Console.WriteLine("Would you like to play again?");
                     FinalAwn = Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                 }
                 while (FinalAwn == "yes");
@@ -238,10 +234,10 @@ namespace ConsoleApplication3
 }
 
 
-        
-    
 
-    
+
+
+
 
 
 
